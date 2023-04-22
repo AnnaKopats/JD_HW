f9ccb81dc21d410cf46f65671ee61f660688974c
Anna Kopats <yampolya85@gmail.com>
2023-04-22 11:02:56 +0300

    new commit 2.04 Д/з 3: исправила задачи 4-7. Д/з 4 решена зад. 3

--- src/com/academy/kopats/lesson4/Task3.java
+++ src/com/academy/kopats/lesson4/Task3.java
@@ -1,10 +1,28 @@
 package com.academy.kopats.lesson4;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task3 {
-    public static void main(String[] args) {
-
+ public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+     for (int i = 0; i < a; i++) {
+         System.out.print(fibonacci(i)+ "");
+     }
 
     }
+
+
+    public static int fibonacci(int f) {
+
+        if (f == 0) {
+            return 0;
+        } else if (f == 1 || f == 2) {
+            return 1;
+        } else
+            return fibonacci(f - 1) + fibonacci(f - 2);
+    }
+
+
 }

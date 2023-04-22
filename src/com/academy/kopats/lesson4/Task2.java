b408d3ab777154b454019a68707cea524679d46d
Anna Kopats <yampolya85@gmail.com>
2023-04-22 10:51:39 +0300

    new commit 2.04 Д/з 3: исправила задачи 4-7. Д/з 4 решена зад. 2

--- src/com/academy/kopats/lesson4/Task2.java
+++ src/com/academy/kopats/lesson4/Task2.java
@@ -4,6 +4,18 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите два числа: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.print("Наибольший общий делитель этих чисел равен: " + MaxDivisor(a, b));
+    }
+
+    public static int MaxDivisor(int n, int m) {
+        if (n == m) return n;
+        else if (n > m) return MaxDivisor(n - m, m);
+        else
+            return MaxDivisor(n, m - n);
     }
 }
 

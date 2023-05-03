d59240a719ace3856af99395cb71e1f1f2187984
Anna Kopats <yampolya85@gmail.com>
2023-05-04 00:08:00 +0300

    new commit 03.05 Домашка 3 задачи со звездочкой

--- src/com/academy/kopats/lesson3/Task9.java
+++ src/com/academy/kopats/lesson3/Task9.java
@@ -3,5 +3,24 @@ package com.academy.kopats.lesson3;
 public class Task9 {
     public static void main(String[] args) {
 
+        for (int i = 0; i <= 9; i++) {
+            System.out.print(i);
+            if (i == 9) {
+                for (i = 0; i <= 9; i++) {
+                    System.out.print(i + "" + i);
+                    if (i == 9) {
+                        for (i = 0; i <= 9; i++) {
+                            System.out.print(i + "" + i + "" + i);
+                            if (i == 9) {
+                                for (i = 0; i <= 9; i++) {
+                                    System.out.print(i + "" + i + "" + i + "" + i);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }

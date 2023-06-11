1f964083a3307afc17d217f19719accd44585d80
Anna Kopats <yampolya85@gmail.com>
2023-06-12 00:38:56 +0300

    new commit 12.06 Домашка 4 Задачи 6 и 7 со звездочкой

--- src/com/academy/kopats/lesson4/Task6.java
+++ src/com/academy/kopats/lesson4/Task6.java
@@ -1,4 +1,42 @@
 package com.academy.kopats.lesson4;
 
+import java.util.Arrays;
+
 public class Task6 {
+    public static void main(String[] args) {
+        int[] array = {3, 0, 9, 22, 74, 11, 36, 2};
+        System.out.println("Изначальный массив:" + Arrays.toString(array));
+        int low = 0;
+        int high = array.length - 1;
+        quickSort(array, low, high);
+        System.out.println("Отсортированный массив: " + Arrays.toString(array));
+    }
+    public static void quickSort(int[] array, int low, int high) {
+        if (array.length == 0)
+            return;
+        if (low >= high)
+            return;
+        int average = low + (high - low) / 2;
+        int support = array[average];
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < support) {
+                i++;
+            }
+            while (array[j] > support) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if (low < j)
+            quickSort(array, low, j);
+        if (high > i)
+            quickSort(array, i, high);
+    }
 }
--- src/com/academy/kopats/lesson4/Task7.java
+++ src/com/academy/kopats/lesson4/Task7.java
@@ -1,4 +1,36 @@
 package com.academy.kopats.lesson4;
+import java.text.DecimalFormat;
 
 public class Task7 {
+    public static void main(String[] args) {
+        double[] arr = new double[20];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Math.cbrt(Math.pow(((Math.random() * (9 - 5.33 + 1)) + 5.33), 2) + 4.5);
+        }
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.printf("|  A[0] = %.4f  A[1] = %.4f  A[1] = %.4f  A[3] = %.4f A[4] = %.4f    |\n", arr[0], arr[1], arr[2], arr[3], arr[4]);
+        System.out.printf("|  A[5] = %.4f  A[6] = %.4f  A[7] = %.4f  A[8] = %.4f A[9] = %.4f    |\n", arr[5], arr[6], arr[7], arr[8], arr[9]);
+        System.out.printf("|  A[10] = %.4f A[11] = %.4f A[12] = %.4f A[13] = %.4f A[14] = %.4f  |\n", arr[10], arr[11], arr[12], arr[13], arr[14]);
+        System.out.printf("|  A[15] = %.4f A[16] = %.4f A[17] = %.4f A[18] = %.4f A[19] = %.4f  |\n", arr[15], arr[16], arr[17], arr[18], arr[19]);
+
+        int i;
+        DecimalFormat dF = new DecimalFormat( "###.###" );
+        double resMultiply = 1;
+        for ( i= 0; i < arr.length; i++) {
+            arr[i] = Math.random() * 7.5 + 3.5;
+            resMultiply *= arr[i];
+        }
+        double geometricMean = Math.pow(resMultiply, 1.0 / arr.length);
+        System.out.printf("Среднее геометрическое нового массива равно " + dF.format(geometricMean));
+        System.out.println();
+        System.out.println("-------------------------------------------------------------------------------");
+
+    }
 }
+
+
+
+
+
+
+//          System.out.print(dF.format(arr[i]) + " ");

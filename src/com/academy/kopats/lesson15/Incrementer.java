8fc9ff22bc9c21a067b2017ba399cbc0d80b5881
Valery Borys <valeryborys@gmail.com>
2023-06-13 20:09:20 +0300

    check15

--- src/com/academy/kopats/lesson15/Incrementer.java
+++ src/com/academy/kopats/lesson15/Incrementer.java
@@ -7,7 +7,7 @@ public class Incrementer extends Thread {
         for (int i = 0; i < 500; i++) {
             Task3.value++;
 
-            while (!Thread.currentThread().isInterrupted()) {
+            while (!Thread.currentThread().isInterrupted()) { //прочитай внимательно. пока поток не прерван - выбрасывай исключение. наоборот прервать нужно каждый 10-ый
 
                 throw new RuntimeException("Возникло исключение" + Thread.currentThread().getName());
 

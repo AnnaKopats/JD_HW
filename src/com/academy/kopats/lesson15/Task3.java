95c21d8f2d2270b25e2a31541cbfec0a0ee64721
Anna Kopats <yampolya85@gmail.com>
2023-06-14 01:18:30 +0300

    new commit Домашка 15 исправления

--- src/com/academy/kopats/lesson15/Task3.java
+++ src/com/academy/kopats/lesson15/Task3.java
@@ -6,21 +6,22 @@ import java.util.List;
 public class Task3 {
     public static Integer value = 0;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         List<Incrementer> list = new ArrayList<>();
         for (int i = 0; i < 1000; i++) {
+            if (i % 10 == 0) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Возникло исключение " + Thread.currentThread().getName() + " " + i);
+            }
             Incrementer incrementer = new Incrementer();
             incrementer.start();
             list.add(incrementer);
-
         }
-
         for (Incrementer incrementer : list) {
-            int counter = 0;
-            counter++;
-            if (counter%10==0){
-                incrementer.interrupt();
-            }
             try {
                 incrementer.join();
             } catch (InterruptedException e) {

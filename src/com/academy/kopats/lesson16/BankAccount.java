a880f0ea119be9c27cc2de3f8d3659c1d0999023
Anna Kopats <yampolya85@gmail.com>
2023-06-19 23:00:45 +0300

    new commit Домашка 16 исправления

--- src/com/academy/kopats/lesson16/BankAccount.java
+++ src/com/academy/kopats/lesson16/BankAccount.java
@@ -1,88 +1,27 @@
 package com.academy.kopats.lesson16;
 
 public class BankAccount {
-    public static Double currentBalance = 300.00;
-    public static final Double REPLENISHED = 100.00;
-    public static final Double DEBITING_FUNDS = 200.00;
-    public static Integer count = 100;
-    public static Integer count2 = 0;
-    public static Integer count3 = 0;
-    private static final BankAccountDebitingFundsBalance BADFB = new BankAccountDebitingFundsBalance();
-    private static final BankAccountReplenishedBalance BARB = new BankAccountReplenishedBalance();
+    private Double currentBalance;
 
-
-    private static void stopIt(Object o) {
-        synchronized (o) {
-            try {
-                o.wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    private static void wakeUp(Object o) {
-        synchronized (o) {
-            o.notify();
-        }
+    public BankAccount(Double currentBalance) {
+        this.currentBalance = currentBalance;
     }
 
 
-    public static class BankAccountDebitingFundsBalance extends Thread {
-
-        public synchronized void debitingFundsBalance() {
-            currentBalance -= DEBITING_FUNDS;
-        }
-
-        @Override
-        public void run() {
-            synchronized (BARB) {
-                while (count > 0) {
-                    synchronized (this) {
-                        if (count2 ==0) {
-                            stopIt(BADFB);
-                        }else {
-                            debitingFundsBalance();
-                            count2++;
-                            System.out.println(getName() + " Списано: " + DEBITING_FUNDS + "Текущий баланс: " + currentBalance);
-                        }
-                    }
-                    synchronized (BARB) {
-                        if (count2 == 1) {
-                            wakeUp(BARB);
-                        }
-                    }
-                }
-            }
-        }
+    public synchronized void debitingFundsBalance(double amount) {
+        currentBalance -= amount;
+        System.out.println("Списано: " + amount + ", текущий баланс: " + currentBalance);
     }
 
-    public static class BankAccountReplenishedBalance extends Thread {
 
-        public synchronized void replenishedBalance() {
-            currentBalance += DEBITING_FUNDS;
-            currentBalance += DEBITING_FUNDS;
-        }
+    public synchronized void replenishedBalance(double amount) {
+        currentBalance += amount;
+        System.out.println("Зачислено: " + amount + ", текущий баланс: " + currentBalance);
+    }
 
-        @Override
-        public void run() {
-            synchronized (BADFB) {
-                while (count > 0) {
-                    synchronized (this) {
-                        if (count3 == 0)
-                            stopIt(BARB);
-                    }
-                    synchronized (BADFB) {
-                        replenishedBalance();
-                        count3++;
-                        System.out.println(getName() + " Поступило: " + REPLENISHED + "Текущий баланс: " + currentBalance);
-                        if (count3 == 1) {
-                            wakeUp(BADFB);
-                        }
-                    }
-                }
-            }
-        }
+    @Override
+    public String toString() {
+        return currentBalance.toString();
     }
 }
 
--- src/com/academy/kopats/lesson16/Task1.java
+++ src/com/academy/kopats/lesson16/Task1.java
@@ -6,12 +6,27 @@ import static java.lang.Thread.sleep;
 
 public class Task1 {
     public static void main(String[] args) throws InterruptedException {
-        new BankAccount.BankAccountDebitingFundsBalance().start();
-
-        new BankAccount.BankAccountReplenishedBalance().start();
-
-        Thread.sleep(1000);
-        System.out.println(BankAccount.currentBalance);
-
+        BankAccount bankAccount = new BankAccount(300.00);
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 200; i++) {
+                    bankAccount.replenishedBalance(100.00);
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    bankAccount.debitingFundsBalance(200.00);
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println("Текущий баланс равен: " + bankAccount);
     }
 }

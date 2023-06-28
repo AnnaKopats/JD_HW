b1d2f0abd896a4c8f0bebd03c5097e73e5aa6a70
Anna Kopats <yampolya85@gmail.com>
2023-06-29 00:54:56 +0300

    new commit Домашка 8 задача со *

--- src/com/academy/kopats/lesson8/Task6.java
+++ src/com/academy/kopats/lesson8/Task6.java
@@ -1,5 +1,20 @@
 package com.academy.kopats.lesson8;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task6 {
     //(\W|^)[\w.\-]{0,25}@(yahoo|hotmail|gmail)\.com(\W|$)
+    public static void main(String[] args) {
+        printEmail("If you were able to connectthena-win@gmail.com will appear in front of you to enter your login and password annakopats@mail.ru");
+    }
+
+    public static void printEmail(String s) {
+        Pattern pattern = Pattern.compile("([A-zА-я])+([0-9\\-_\\+\\.])*([A-zА-я0-9\\-_\\+\\.])*@([A-zА-я])+([0-9\\-_\\+\\.])*([A-zА-я0-9\\-_\\+\\.])*[\\.]([A-zА-я])+");
+        Matcher matcher = pattern.matcher(s);
+        while (matcher.find()) {
+            System.out.print(matcher.group() + "; ");
+
+        }
+    }
 }
